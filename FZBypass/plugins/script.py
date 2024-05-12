e9aa8a190c5b89e80e75e8ebd6ed0512b4b0e4bc
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-05-12 08:50:03 +0530

    Update script.py

--- FZBypass/plugins/script.py
+++ FZBypass/plugins/script.py
@@ -4,105 +4,105 @@ class script(object):
 <b>Here Is The Supported Site List.</b>"""
   
   SCRAPE_TXT="""`cinevood.*`✅️ \n
- `kayoanime.com` ✅️ \n
-`skymovieshd.*`✅️ \n
-`toonworld4all.*`❌️ \n
-`ww1.sharespark.cfd`✅️ \n
-`www.1tamilmv.*`✅️ \n"""
+ kayoanime.com ✅️ \n
+skymovieshd.*✅️ \n
+toonworld4all.*❌️ \n
+ww1.sharespark.cfd✅️ \n
+www.1tamilmv.*✅️ \n"""
   SHORTENER_TXT="""`adrinolinks.com`✅️ \n
-`adsfly.in`✅️ \n
-`anlinks.in`️✅️ \n
-`appurl.io`✅️ \n
-`bindaaslinks.com`✅️ \n
-`bit.ly` + `tinyurl.com` + `*.short.gy` + `shorturl.ac` + `t.ly`✅️ \n
-`bringlifes.com`️️⚠️ \n
-`dalink.in`️⚠️ \n
-`disk.yandex.ru` + `yandex.com`✅️ \n
-`download.mdiskshortner.link`✅️ \n
-`droplink.co`✅️ \n
-`dtglinks.in`✅️ \n
-`du-link.in` + `dulink.in`✅️ \n
-`earn.moneykamalo.com`✅️ \n
-`earn2me.com`✅️ \n
-`earn2short.in`✅️ \n
-`earn4link.in`✅️ \n
-`evolinks.in`✅ \n
-`ez4short.com`✅️ \n
-`go.lolshort.tech`❌️ \n
-`gtlinks.me` + `gyanilinks.com`✅ \n
-`indianshortner.in`✅️ \n
-`indyshare.net`✅️ \n
-`instantearn.in`✅️ \n
-`kpslink.in`✅️ \n
-`krownlinks.me`✅️ \n 
-`link.shorito.com`❌️ \n
-`link.tnlink.in`️✅️ \n
-`link.tnshort.net`✅️ \n
-`link.vipurl.in` + `vipurl.in` + `count.vipurl.in`✅️ \n
-`link1s.com`✅️ \n
-`link4earn.com` + `link4earn.in`✅️ \n
-`linkbanao.com`❌️ \n
-`linkfly.me`✅️ \n
-`linkjust.com`✅️ \n
-`linkpays.in`✅️ \n
-`linkshortx.in`✅️ \n
-`linksly.co`✅️ \n
-`linkvertise.com`️❌️ \n
-`linkyearn.com`❌️ \n
-`m.easysky.in`✅ \n
-`m.narzolinks.click`✅️ \n 
-`mdisk.pro`✅️ \n
-`mdiskshortner`✅ \n
-`mediafire.com`✅️ \n
-`modijiurl.com`️❌️ \n
-`moneycase.link`✅️ \n
-`mplaylink.com`️❌️ \n
-`omnifly.in.net`✅️ \n
-`onepagelink.in`✅️ \n
-`ouo.io` + `ouo.press`✅️ \n
-`pandaznetwork.com`✅️ \n
-`pdisk.site`✅️ \n
-`pdiskshortener.com`✅️ \n
-`pkin.me` + `go.paisakamalo.in`✅️ \n
-`publicearn.com`❌️ \n
-`rocklinks.net`✅️ \n
-`ronylink.com`✅️ \n
-`rslinks.net`❌️ \n
-`sheralinks.com`✅️ \n
-`short.tnvalue.in`✅️ \n
-`short2url.in`✅️ \n
-`shortingly.com`️✅️️ \n
-`shrdsk.me`️️✅️ \n
-`shrinke.me`✅️ \n
-`shrinkforearn.xyz`️❌️ \n
-`sklinks.in` + `sklinks.tech`✅️ \n
-`surl.li`✅️ \n
-`sxslink.com`✅️ \n
-`tamizhmasters.com`⚠️ \n
-`terabox.*` + `terabox.*` + `nephobox.*` + `4funbox.*` + `mirrobox.*` + `momerybox.*` + `teraboxapp.*`✅️ \n
-`tglink.in`✅️ \n
-`tinyfy.in`✅️ \n
-`try2link.com`✅️ \n
-`tulinks.one` + `go.tulinks.online` + `tulinks.online`✅️ \n
-`url4earn.in`✅️ \n
-`urllinkshort.in`✅️ \n
-`urlsopen.com`✅️ \n
-`urlspay.in`✅️ \n
-`v2.kpslink.in`✅️ \n 
-`v2links.com`️❌️ \n
-`viplinks.io`️⚠️ \n
-`vplinks.in`✅️ \n
-`www.dropbox.com`✅️ \n
-`xpshort.com` + `push.bdnewsx.com` + `techymozo.com`❌ \n
+adsfly.in✅️ \n
+anlinks.in✅️ \n
+appurl.io✅️ \n
+bindaaslinks.com✅️ \n
+bit.ly + tinyurl.com + *.short.gy + shorturl.ac + t.ly✅️ \n
+bringlifes.com⚠️ \n
+dalink.in⚠️ \n
+disk.yandex.ru + yandex.com✅️ \n
+download.mdiskshortner.link✅️ \n
+droplink.co✅️ \n
+dtglinks.in✅️ \n
+du-link.in + dulink.in✅️ \n
+earn.moneykamalo.com✅️ \n
+earn2me.com✅️ \n
+earn2short.in✅️ \n
+earn4link.in✅️ \n
+evolinks.in✅ \n
+ez4short.com✅️ \n
+go.lolshort.tech❌️ \n
+gtlinks.me + gyanilinks.com✅ \n
+indianshortner.in✅️ \n
+indyshare.net✅️ \n
+instantearn.in✅️ \n
+kpslink.in✅️ \n
+krownlinks.me✅️ \n 
+link.shorito.com❌️ \n
+link.tnlink.in✅️ \n
+link.tnshort.net✅️ \n
+link.vipurl.in + vipurl.in + count.vipurl.in✅️ \n
+link1s.com✅️ \n
+link4earn.com + link4earn.in✅️ \n
+linkbanao.com❌️ \n
+linkfly.me✅️ \n
+linkjust.com✅️ \n
+linkpays.in✅️ \n
+linkshortx.in✅️ \n
+linksly.co✅️ \n
+linkvertise.com❌️ \n
+linkyearn.com❌️ \n
+m.easysky.in✅ \n
+m.narzolinks.click✅️ \n 
+mdisk.pro✅️ \n
+mdiskshortner✅ \n
+mediafire.com✅️ \n
+modijiurl.com❌️ \n
+moneycase.link✅️ \n
+mplaylink.com❌️ \n
+omnifly.in.net✅️ \n
+onepagelink.in✅️ \n
+ouo.io + ouo.press✅️ \n
+pandaznetwork.com✅️ \n
+pdisk.site✅️ \n
+pdiskshortener.com✅️ \n
+pkin.me + go.paisakamalo.in✅️ \n
+publicearn.com❌️ \n
+rocklinks.net✅️ \n
+ronylink.com✅️ \n
+rslinks.net❌️ \n
+sheralinks.com✅️ \n
+short.tnvalue.in✅️ \n
+short2url.in✅️ \n
+shortingly.com✅️️ \n
+shrdsk.me✅️ \n
+shrinke.me✅️ \n
+shrinkforearn.xyz❌️ \n
+sklinks.in + sklinks.tech✅️ \n
+surl.li✅️ \n
+sxslink.com✅️ \n
+tamizhmasters.com⚠️ \n
+terabox.* + terabox.* + nephobox.* + 4funbox.* + mirrobox.* + momerybox.* + teraboxapp.*✅️ \n
+tglink.in✅️ \n
+tinyfy.in✅️ \n
+try2link.com✅️ \n
+tulinks.one + go.tulinks.online + tulinks.online✅️ \n
+url4earn.in✅️ \n
+urllinkshort.in✅️ \n
+urlsopen.com✅️ \n
+urlspay.in✅️ \n
+v2.kpslink.in✅️ \n 
+v2links.com❌️ \n
+viplinks.io⚠️ \n
+vplinks.in✅️ \n
+www.dropbox.com✅️ \n
+xpshort.com + push.bdnewsx.com + techymozo.com❌ \n
 `ziplinker.net`✅️ \n"""
-  GDRIVE_TXT="""`appdrive.*`✅️ \n
-`drivefire.co`✅️ \n
-`*.gdflix.*` ❌️ \n
-`hubdrive.lat` \n
-`katdrive.org`✅️ \n
-`new*.gdtot.*`|❌️ \n
-`new*.filepress.store` + `filebee.xyz` + `onlystream.xyz` + `pressbee.xyz`✅️ \n
-`sharer.pw`❌"""
+  GDRIVE_TXT="""appdrive.*✅️ \n
+drivefire.co✅️ \n
+*.gdflix.* ❌️ \n
+hubdrive.lat \n
+katdrive.org✅️ \n
+new*.gdtot.*❌️ \n
+new*.filepress.store + filebee.xyz + onlystream.xyz + pressbee.xyz✅️ \n
+sharer.pw❌"""
 
 
 
